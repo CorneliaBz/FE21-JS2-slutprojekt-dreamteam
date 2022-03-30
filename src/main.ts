e1bf002f8e600b46dc92b6420dc21cfddf6d1f1f
CorneliaBz <bergentz2@hotmail.com>
2022-03-30 14:58:08 +0200

    Added function to not create a user with the same username

--- src/main.ts
+++ src/main.ts
@@ -4,50 +4,70 @@ import { User } from "./modules/User";
 
 console.log(db);
 const dbRef = ref(db, '/User');
-let user:User[] = [];
+let user: User[] = [];
 
-onValue(dbRef, snapshot=>{
-    const messageData = snapshot.val();
-    console.log(messageData); 
+onValue(dbRef, snapshot => {
+    const userData = snapshot.val();
+    console.log(userData);
 
     user = [];
-    for(const key in messageData){
+    for (const key in userData) {
         user.push(new User(
             key,
-            messageData[key].bio,
-            messageData[key].color,
-            messageData[key].name,
-            messageData[key].password,
+            userData[key].bio,
+            userData[key].color,
+            userData[key].name,
+            userData[key].password,
         ))
     }
-})
+//Koden för att skapa en ny User
+    function createUser(): any {
+        //Hämtar inputElement
+        const name: HTMLInputElement = document.querySelector('#newUserName');
+        const password: HTMLInputElement = document.querySelector('#newUserPassword');
+        const confirmPassword: HTMLInputElement = document.querySelector('#confirmNewUserPassword');
+        //Felmeddelande
+        const message: HTMLHeadElement = document.querySelector('#loginMessage');
+        //Variabler som ska jämföras med varandra
+        const newUsername = name.value;
+        const userNames = Object.values(userData);
+        
+        let addUser = true;
 
-//Skapar användare
-document.querySelector('#signupButton').addEventListener('click', e=>{
-    e.preventDefault();
-    const name: HTMLInputElement = document.querySelector('#newUserName');
-    const password: HTMLInputElement = document.querySelector('#newUserPassword');
-    const confirmPassword: HTMLInputElement = document.querySelector('#confirmNewUserPassword');
-    
-    const message: HTMLHeadElement = document.querySelector('#loginMessage');
-
-    console.log(name.value, password.value)
-    //if-statement som kollar om lösenorden stämmer överrens
-    if(password.value == confirmPassword.value){
-        const UserToAdd = {
-            bio: '',
-            color: '',
-            name: name.value,
-            password: password.value,
+        for(const userName of userNames){
+            //Kollar om newUsername finns i databasen databasen som userName.name. 
+            //Om namnet redan finns kan vi inte skapa användare då addUser = false.
+            if(newUsername === userName.name){
+                addUser = false;
+                message.innerText = 'User already exsists';
+                break;
+            }
         }
+        //Om newUsername och userName.name inte är samma skapas en ny användare.
+        //Här kollas även om lösenordet matchar varandra.
+        if(addUser && password.value == confirmPassword.value){
+            const UserToAdd = {
+                bio: '',
+                color: '',
+                name: name.value,
+                password: password.value,
+            }
 
-        const newKey:string = push(dbRef).key;
-        const newUser = {};
-        newUser[newKey] = UserToAdd;
+            const newKey: string = push(dbRef).key;
+            const newUser = {};
+            newUser[newKey] = UserToAdd;
 
-        update(dbRef, newUser);
-        message.innerText = 'New user created, you can now sign in';
-    }else{
-        message.innerText = 'Password not matching';
-    };
-})
+            update(dbRef, newUser);
+            message.innerText = 'New user created, you can now sign in';
+        //Felmeddelande ifall lösenorden inte är samma.
+        }else if(addUser && password.value != confirmPassword.value){
+            message.innerText = 'Password not matching';
+        }
+    }
+
+    //Knapp som kallar på createUser();
+    document.querySelector('#signupButton').addEventListener('click', e => {
+        e.preventDefault();
+        createUser();
+    })
+});
