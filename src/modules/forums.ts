3f2fe169bafcd69b6c4be1bd41b9f2b6b2fe8322
LinaRiddersporre <85893597+LinaRiddersporre@users.noreply.github.com>
2022-03-30 19:47:58 +0200

    create input
    and fixed the conection with the database

--- src/modules/forums.ts
+++ src/modules/forums.ts
@@ -1,18 +1,31 @@
-const body = document.querySelector('body')
-function createDivs(){
-    const createWrapperDiv = document.createElement('div');
-    body.appendChild(createWrapperDiv);
-    createWrapperDiv.style.border ='solid 2px black';
-    createWrapperDiv.style.padding = '2rem';
-    function createPost(){
+import {db} from "./firebaseApp";
+import { onValue, ref } from "firebase/database";
+
+
+const dbRef = ref (db, '/Forum/topic1');
+console.log(dbRef)
+
+onValue(dbRef, snapshot=>{
+    console.log(snapshot);
+    console.log(snapshot.val());
+    createDivs(snapshot.val());
+});
+
+function createDivs(products){
+    for(const key in products){
+        const createWrapperDiv = document.createElement('div');
+        document.body.append(createWrapperDiv);
+        createWrapperDiv.style.border ='solid 2px black';
+        createWrapperDiv.style.padding = '2rem';
+        const createNameDiv = document.createElement('div');
+        createWrapperDiv.appendChild(createNameDiv);
+        createNameDiv.innerText = products[key].name;
         const createPostDiv = document.createElement('div');
         createWrapperDiv.appendChild(createPostDiv);
         createPostDiv.style.border ='solid 2px black';
-        createPostDiv.innerText = 'Ett nytt Test'
+        createPostDiv.innerText = products[key].message;
     }
-    createPost();
-    createPost();
-}
+};
 
 
 
