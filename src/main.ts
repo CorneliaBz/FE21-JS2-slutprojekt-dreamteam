9701e8adc56f8bf5c1f89da8dcfbaa6104e60e19
CorneliaBz <bergentz2@hotmail.com>
2022-04-01 13:55:05 +0200

    Cleaned up main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import { onValue, ref, push, update, remove } from "firebase/database";
 import { db } from "./modules/firebaseapp";
-import { User } from "./modules/User";
+import { User, createUser } from "./modules/User";
 import { createDivs } from "./modules/forums";
 
-createDivs();
+// createDivs();
 
 console.log(db);
 const dbRef = ref(db, '/User');
 let user: User[] = [];
+let userData;
 
 onValue(dbRef, snapshot => {
-    const userData = snapshot.val();
+    userData = snapshot.val();
     console.log(userData);
 
     user = [];
@@ -23,54 +24,10 @@ onValue(dbRef, snapshot => {
             userData[key].password,
         ))
     }
-//Koden för att skapa en ny User
-    function createUser(): any {
-        //Hämtar inputElement
-        const name: HTMLInputElement = document.querySelector('#newUserName');
-        const password: HTMLInputElement = document.querySelector('#newUserPassword');
-        const confirmPassword: HTMLInputElement = document.querySelector('#confirmNewUserPassword');
-        //Felmeddelande
-        const regMessage: HTMLHeadElement = document.querySelector('#regMessage');
-        //Variabler som ska jämföras med varandra
-        const newUsername = name.value;
-        const userNames = Object.values(userData);
-        
-        let addUser = true;
-
-        for(const userName of userNames){
-            //Kollar om newUsername finns i databasen databasen som userName.name. 
-            //Om namnet redan finns kan vi inte skapa användare då addUser = false.
-            if(newUsername === userName.name){
-                addUser = false;
-                regMessage.innerText = 'Användaren finns redan';
-                break;
-            }
-        }
-        //Om newUsername och userName.name inte är samma skapas en ny användare.
-        //Här kollas även om lösenordet matchar varandra.
-        if(addUser && password.value == confirmPassword.value){
-            const UserToAdd = {
-                bio: '',
-                color: '',
-                name: name.value,
-                password: password.value,
-            }
-
-            const newKey: string = push(dbRef).key;
-            const newUser = {};
-            newUser[newKey] = UserToAdd;
-
-            update(dbRef, newUser);
-            regMessage.innerText = 'Ny användare skapad, du kan logga in';
-        //Felmeddelande ifall lösenorden inte är samma.
-        }else if(addUser && password.value != confirmPassword.value){
-            regMessage.innerText = 'Password not matching';
-        }
-    }
 
     //Knapp som kallar på createUser();
     document.querySelector('#signupButton').addEventListener('click', e => {
         e.preventDefault();
-        createUser();
+        createUser(userData);
     })
 });
