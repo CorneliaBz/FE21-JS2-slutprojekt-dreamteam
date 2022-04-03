a9c01b51c9022b8d2f1cac65cdb8e09fc9ec8f48
LinaRiddersporre <85893597+LinaRiddersporre@users.noreply.github.com>
2022-04-03 14:30:27 +0200

    Add message to forum

--- src/modules/forums.ts
+++ src/modules/forums.ts
@@ -1,20 +1,46 @@
 import {db} from "./firebaseApp";
-import { onValue, ref } from "firebase/database";
+import { onValue, push, ref, update } from "firebase/database";
 
 
 const dbRef = ref (db, '/Forum/topic1');
 console.log(dbRef)
 
+class Posts{
+    constructor(
+        public readonly id: string,
+        public readonly message: string,
+        public readonly name:string
+    ){
+
+    }
+}
+
+let newPost:Posts[] = [];
+
 onValue(dbRef, snapshot=>{
-    console.log(snapshot);
     console.log(snapshot.val());
     createDivs(snapshot.val());
+    const postData = snapshot.val();
+
+    newPost = [];
+    for(const key in postData){
+        newPost.push(new Posts(
+            key,
+            postData[key].message,
+            postData[key].name
+        ))
+    }
+
+
 });
 
+
+const postWrapper = document.querySelector('#postWrapper');
 function createDivs(products){
+    
     for(const key in products){
         const createWrapperDiv = document.createElement('div');
-        document.body.append(createWrapperDiv);
+        postWrapper.append(createWrapperDiv);
         createWrapperDiv.style.border ='solid 2px black';
         createWrapperDiv.style.padding = '2rem';
         const createNameDiv = document.createElement('div');
@@ -31,6 +57,27 @@ function createDivs(products){
     }
 };
 
+document.querySelector('#sendMessageToForum').addEventListener('click', event=>{
+    event.preventDefault();
+    console.log('knapp fungerar')
+
+    postWrapper.innerHTML = '';
+    // const getUser = document.querySelector('#userName');
+    const messageToForum = document.querySelector('#messageToForum');
+    const messageValue = messageToForum.value;
+    const userValue = 'getUser.value';
+
+    console.log(userValue, messageValue);
+    const postToAdd = {
+        message: messageValue,
+        name: userValue
+    }
+    const newMessageKey:string = push(dbRef).key;
+    const createNewPost = {};
+    createNewPost[newMessageKey] = postToAdd;
+    update(dbRef, createNewPost); 
+    
+})
 
 
 export {createDivs}
