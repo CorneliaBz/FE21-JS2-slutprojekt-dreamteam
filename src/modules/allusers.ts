f991e61ef4c74ce5d27290eaf6efeaaf42a0041e
BennyGRIT <benny.andersson@gritacademy.se>
2022-04-05 23:47:22 +0200

    Commiting working showUsers

--- src/modules/allusers.ts
+++ src/modules/allusers.ts
@@ -1,7 +1,7 @@
 
 import { onValue, ref, push, update, remove } from "firebase/database";
 import { db } from "./firebaseapp";
-import { User, createUser } from "./User";
+import { User } from "./User";
 
 
 
@@ -20,29 +20,29 @@ export function showAllUsersFunction(allUsers) {
         const userContainer: any = document.getElementById('anyUserContainer')
         const showUserBio: any = document.getElementById('showUserBio')
 
-        for (let i=0; i<userNames.length; i++){
-            let alluserP: any = document.createElement('p');
-            alluserP.setAttribute('id', 'p'+[i])
-            const userNameList:any = userNames[i].name
-            alluserP.innerText = userNames[i].name;
+        for (let i = 0; i < userNames.length; i++) {
+            let alluserBtn: any = document.createElement('button');
+            alluserBtn.setAttribute('id', 'btn' + [i])
+            const userNameList: any = userNames[i].name
+            alluserBtn.innerText = userNames[i].name;
 
-            const thisUser = document.getElementById('p'+[i]);
-            // thisUser.setAttribute('href',showUserBio )
-            // userNameList.setAttribute('href', showUserBio )
-            userContainer.appendChild(alluserP)}
+            console.log(alluserBtn)
+            userContainer.appendChild(alluserBtn)
 
+            const specificUersBtn:any = document.getElementById('btn'+[i])
+            console.log(specificUersBtn)
+            console.log(typeof specificUersBtn)
 
+            specificUersBtn.addEventListener('click', ()=>{
+                const showNameP = document.getElementById('showNameP');
+                const showImgP = document.getElementById('showImgP');
+                const showBioP = document.getElementById ('showBioP');
+                showNameP.innerHTML = userNames[i].name
+                showImgP.innerHTML = userNames[i].img
+                showBioP.innerHTML = userNames[i].bio
 
+            })
+        }
     })
 
 }
-//     user = [];
-//     for (const key in userData) {
-//         user.push(new User(
-//             key,
-//             userData[key].bio,
-//             userData[key].img,
-//             userData[key].name,
-//             userData[key].password,
-//         ))
-//     }
