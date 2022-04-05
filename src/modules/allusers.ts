814c8f939abe60630b0ddf685c9b2b8613802101
BennyGRIT <benny.andersson@gritacademy.se>
2022-04-05 22:09:40 +0200

    Show all names OK

--- src/modules/allusers.ts
+++ src/modules/allusers.ts
@@ -5,34 +5,25 @@ import { User, createUser } from "./User";
 
 
 
+export function showAllUsersFunction(allUsers) {
+    const dbRef = ref(db, '/User');
+    let user: User[] = [];
+    let userData;
 
+    onValue(dbRef, snapshot => {
+        userData = snapshot.val();
+        console.log(userData);
 
+        const userNames: User[] = Object.values(userData);
+        console.log(userNames[0].bio)
 
+        const userContainer: any = document.getElementById('anyUserContainer')
 
-
-
-
-export function showAllUsersFunction(){
-const dbRef = ref(db, '/User');
-let user: User[] = [];
-let userData;
-
-onValue(dbRef, snapshot => {
-    userData = snapshot.val();
-    console.log(userData);
-
-    const userNames:User[] = Object.values(userData);
-    console.log(userNames[0].bio)
-
-    const userContainer:any = document.getElementById('anyUserContainer')
-    userContainer.innerHTML= userNames[0].name
-
-    for (let i=0; i<userNames.length; i++){
-        userContainer.innerHTML= userNames[i].name
-        console.log(userNames[i].name)
-    }
-
-})
+        for (let i=0; i<userNames.length; i++){
+            let alluserP: HTMLParagraphElement = document.createElement('p');
+            alluserP.innerText = userNames[i].name + 'Bennyyyy';
+            userContainer.appendChild(alluserP)}
+    })
 }
 //     user = [];
 //     for (const key in userData) {
@@ -44,33 +35,3 @@ onValue(dbRef, snapshot => {
 //             userData[key].password,
 //         ))
 //     }
-
-//     //Knapp som kallar på createUser() från ./modules/User;
-//     document.querySelector('#signupButton').addEventListener('click', e => {
-//         e.preventDefault();
-//         createUser(userData);
-//     })
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-// Funktion för att visa allas Bio sida via forumet:
-// export class AllUsers {
-//     constructor(
-//        public readonly allUsers: Object,
-//     ) {
-//         this.displayAllTheUsers(allUsers);
-//     }
-
-//     private displayAllTheUsers(allUsers):void{
-
-//     }}
